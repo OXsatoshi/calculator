0afffae82697075c802adee76047464a66a7decd
OXsatoshi <afia.nabil24@gmail.com>
2024-04-03 03:52:52 +0100

    Link ui with logic

--- script.js
+++ script.js
@@ -3,11 +3,37 @@ function multiply(a,b){return a*b;}
 function substract(a,b){return a-b;}
 function divide(a,b){return a/b;}
 let operation ;
-let firstOperand;
-let secondOperand;
-const OPERATION ={
+let firstOperand ="";
+let secondOperand="";
+let operationButtonPressed = false;
+const display = document.querySelector(".display");
+function handleClick(e) {
+            if(e.target.classList.contains("operation")){
+                operation = e.target.classList.item(0) ;
+                operationButtonPressed=true;
+            }
+            if(!operationButtonPressed){
+                firstOperand+=e.target.textContent;
+                display.textContent= firstOperand;
+            }        
+            else{
+                secondOperand+=e.target.textContent;
+            }
+            console.log(operation);
 
-};
+        
+    
+    
+}
+function handleEqual(){
+    let result = operate(operation,parseInt(firstOperand),parseInt(secondOperand));
+    display.textContent = result;
+    operationButtonPressed = false;
+}
+const numberContainer = document.querySelector(".buttons");
+const divsNumbers = document.querySelectorAll("div.buttons > div:not(.equal) ");
+divsNumbers.forEach(div => div.addEventListener("click",handleClick));
+document.querySelector("div.buttons >  div.equal").addEventListener("click",handleEqual);
 function operate(operation,firstOperand,secondOperand){
     const functionOfoperation = getOperationFunction(operation);
     console.log(functionOfoperation);
